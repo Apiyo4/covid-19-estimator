36c21182fe6fa003f8b24145df67032de1988df2
Annette Odingo <odingoannette@gmail.com>
2020-04-11 13:06:50 +0100

    Completed challenge 1

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,16 @@
-const covid19ImpactEstimator = (data) => data;
+const covid19ImpactEstimator = (data) => {
+  const newObj = {
+    data: {},
+    impact: {},
+    severeImpact: {}
+  };
+  newObj.impact['currentlyInfected'] = data.reportedCases * 10;
+  newObj.severeImpact['currentlyInfected'] = data.reportedCases * 50;
+  newObj.impact['infectionsByRequestedTime'] =
+    newObj.impact.currentlyInfected * 2 * Math.pow(2, 9);
+  newObj.severeImpact['infectionsByRequestedTime'] =
+    newObj.severeImpact.currentlyInfected * 2 * Math.pow(2, 9);
+  return newObj;
+};
 
 export default covid19ImpactEstimator;
