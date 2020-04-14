77fea09f75a418fd3addb2f7e08e976db558d7cd
Annette Odingo <odingoannette@gmail.com>
2020-04-14 15:25:47 +0100

    Add data to App.js

--- src/client/src/App.js
+++ src/client/src/App.js
@@ -3,7 +3,36 @@ import React from 'react';
 function App() {
   return (
     <div className="App">
-     <h1>Hello</h1>
+      <h3>Coronavirus disease (COVID‑19)</h3>
+      <div>
+        <h5>Overview</h5>
+        <p>
+          Coronavirus disease causes respiratory illness (like the flu) with
+          symptoms such as a cough, fever, and in more severe cases, difficulty
+          breathing. You can protect yourself by washing your hands frequently,
+          avoiding touching your face, and avoiding close contact (1 meter or 3
+          feet) with people who are unwell.
+        </p>
+        <h5>How it spreads</h5>
+        <p>
+          Coronavirus disease spreads primarily through contact with an infected
+          person when they cough or sneeze. It also spreads when a person
+          touches a surface or object that has the virus on it, then touches
+          their eyes, nose, or mouth.
+        </p>
+        <h5>How to keep safe</h5>
+        <ul>
+          <ol>STAY home as much as you can</ol>
+          <ol>KEEP a safe distance</ol>
+          <ol>WASH hands often</ol>
+          <ol>COVER your cough</ol>
+          <ol>SICK? Call ahead</ol>
+        </ul>
+      </div>
+      <div>
+        <button>Estimator calculator</button>
+        <button>More info</button>
+      </div>
     </div>
   );
 }
