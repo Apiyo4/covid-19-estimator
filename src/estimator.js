afccf6348b158d0bf5ff87cd2440206f3979a502
Annette Odingo <odingoannette@gmail.com>
2020-04-14 03:48:02 +0100

    Fixed eslint problems

--- src/estimator.js
+++ src/estimator.js
@@ -4,11 +4,12 @@ const covid19ImpactEstimator = (data) => {
     impact: {},
     severeImpact: {}
   };
-//   challenge 1
+  //   challenge 1
   newObj.impact.currentlyInfected = data.reportedCases * 10;
   newObj.severeImpact.currentlyInfected = data.reportedCases * 50;
-  newObj.impact.infectionsByRequestedTime = newObj.impact.currentlyInfected * 2 * 2**9;
-  newObj.severeImpact.infectionsByRequestedTime = newObj.severeImpact.currentlyInfected * 2 * 2**9;
+  newObj.impact.infectionsByRequestedTime = newObj.impact.currentlyInfected * 2 * 2 ** 9;
+  newObj.severeImpact.infectionsByRequestedTime = newObj.severeImpact.currentlyInfected
+  * 2 * 2 ** 9;
 
   return newObj;
 };
