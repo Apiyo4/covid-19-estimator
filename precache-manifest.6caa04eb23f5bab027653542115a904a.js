2222e8629072308b5aefd9e237554ef126837be9
Annette Odingo <odingoannette@gmail.com>
2020-04-14 22:29:23 +0100

    Updates

---
+++ precache-manifest.6caa04eb23f5bab027653542115a904a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96f6d02234909570d7060535125f21a8",
+    "url": "/covid-19-estimator/index.html"
+  },
+  {
+    "revision": "13aa4f92c06a958e15eb",
+    "url": "/covid-19-estimator/static/css/main.c4d2c6b5.chunk.css"
+  },
+  {
+    "revision": "da2a27666a4a9b1834ff",
+    "url": "/covid-19-estimator/static/js/2.485dbf84.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/covid-19-estimator/static/js/2.485dbf84.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13aa4f92c06a958e15eb",
+    "url": "/covid-19-estimator/static/js/main.65e67cd0.chunk.js"
+  },
+  {
+    "revision": "36b4733e5ff5dfcd0b0c",
+    "url": "/covid-19-estimator/static/js/runtime-main.d4a58c00.js"
+  }
+]);
