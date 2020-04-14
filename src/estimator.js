df02b27aa4dc02f4bc091774b035e96f567665c4
Annette Odingo <odingoannette@gmail.com>
2020-04-14 10:10:07 +0100

    Finished challenge 2

--- src/estimator.js
+++ src/estimator.js
@@ -13,6 +13,16 @@ const covid19ImpactEstimator = (data) => {
   newObj.severeImpact.infectionsByRequestedTime = newObj.severeImpact.currentlyInfected
   * 2 ** getNumberOfDays(30);
 
+  // challenge 2
+  newObj.impact.severeCasesByRequestedTime = (15 / 100)
+  * newObj.impact.infectionsByRequestedTime;
+  newObj.severeImpact.severeCasesByRequestedTime = (15 / 100)
+  * newObj.severeImpact.infectionsByRequestedTime;
+  newObj.impact.hospitalBedsByRequestedTime = Math.trunc(data.totalHospitalBeds * 0.35)
+    - newObj.impact.severeCasesByRequestedTime;
+  newObj.severeImpact.hospitalBedsByRequestedTime = Math.trunc(data.totalHospitalBeds
+  * 0.35) - newObj.severeImpact.severeCasesByRequestedTime;
+
   return newObj;
 };
 
